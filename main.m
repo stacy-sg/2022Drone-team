7c5b39375316c05ecb9a79d854cd7cc54fdfaaf0
이슬기 <82217475+stacy-sg@users.noreply.github.com>
2022-07-14 00:49:00 +0900

    Update main.m

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ cameraObj = camera(droneObj);
 takeoff(droneObj);
 moveup(droneObj, 'Distance', 0.3);
 
-%원 찾기
+%1단계 원 중점 찾기
 while true
     frame = snapshot(cameraObj);
     hsv_img = rgb2hsv(frame);
@@ -12,25 +12,20 @@ while true
 
     h = hsv_img(:,:,1);
     s = hsv_img(:,:,2);
-    v = hsv_img(:,:,3);
-    threshold = (0.55 < h) & (h < 0.75);
-    threshold_s = (0.43 < s) & (s < 1);
-    %threshold_v = (0.25 < v) & (v < 1);
-    %ring_ = threshold & threshold_s & threshold_v;
+    threshold = (0.575 < h) & (h < 0.675);
+    threshold_s = (0.675 < s) & (s < 0.75);
     ring_ = threshold & threshold_s;
     subplot(2,2,2), subimage(ring_);
     bw =ring_;
-    bw = bwareaopen(bw, 5000);
+    bw = bwareaopen(bw, 3000);
     bw2 = imfill(bw, 'holes');
     
-    %bw2 = bw2 - bw;
     bw2 = ~bw2;
 
     bw2 = bwareaopen(bw2, 1500);
     subplot(2,2,3), subimage(bw2);
 
     canny_img= edge(bw2,'canny');
-    %figure(3),imshow(canny_img);
     subplot(2,2,4), subimage(canny_img);
     hold on
     [w, h] = find(canny_img);
@@ -72,14 +67,11 @@ while true
     green_frame = snapshot(cameraObj);
     hsv_img = rgb2hsv(green_frame);
     h = hsv_img(:,:,1);
-    %s = hsv_img(:,:,2);
-    threshold = (0.3 < h) & (h < 0.35);
-    %threshold_s = (0.6 < s) & (s <= 1);
+    threshold = (0.375 < h) & (h < 0.4);
     green_ = threshold;
     detect_green = bwareaopen(green_, 500);
 
     canny_img= edge(detect_green,'canny');
-    %figure(3),imshow(canny_img);
     [w, h] = find(canny_img);
     y1 = max(w) - min(w);
     x1 = max(h) - min(h);
